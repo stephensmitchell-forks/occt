9c2e67b6daeafdbb666170718aa31b2b2c34b757
nbv <nbv@opencascade.com>
2016-12-15 17:22:00 +0300

    0028222: Intersection of two cylinders fails
    1. The reason of exception has been eliminated.
    
    2. Algorithm in IntPatch_WLineTool::JoinWLines(...) method has been modified in order to forbid join curves in the point where more than two intersection lines meet. More over, joining is forbidden if local curvature in the connection point is too big (see function CheckArgumentsToJoin(...) in the file IntPatch_WLineTool.cxx).
    
    3. Interface of IntPatch_WLineTool::JoinWLines(...) method has been modified in order to reduce number of arguments.
    
    4. Small corrections in IsSeamOrBound(...) static function has been made. Namely, check has been added if two boundaries are in the same period region but are too far each to other (see IntPatch_WLineTool.cxx, IsSeamOrBound(...) function, line # 532).
    
    5. "Reversed" flag has been made local. Now, it is pure local characteristic: the algorithm decides itself, shall we reverse the argument order. This correction makes the algorithm more commutative (see issue #25404). However, IntPatch_WLineTool::JoinWLines(...) method can return non-commutative result.
    
    6. Algorithm of searching small intersection curves has been improved.
    
    7. New methods have been added in Bnd_Range class.
    
    -----------------------------------------
    
    Some test cases have been adjusted according to their new behavior.
    
    1. tests\bugs\modalg_6\bug26310_3
       tests\bugs\modalg_6\bug26310_4
       tests\bugs\moddata_2\bug235
       tests\perf\modalg\bug26310_1
       tests\bugs\modalg_5\bug24915
    
    Logic of these cases has been changed. Mover over, additional check has been added in "bug26310_1" test case. Therefore, its performance will be slower than on the current MASTER.
    
    2. tests\bugs\modalg_5\bug25292*
    
    Scripts have been rewritten in order to make it more readable. Logic of these cases has not been changed.

--- src/IntPatch/IntPatch_WLineTool.hxx
+++ src/IntPatch/IntPatch_WLineTool.hxx
@@ -19,6 +19,8 @@
 #include <IntPatch_WLine.hxx>
 #include <IntPatch_SequenceOfLine.hxx>
 #include <IntSurf_Quadric.hxx>
+#include <Handle_Adaptor3d_HSurface.hxx>
+#include <Handle_Adaptor3d_TopolTool.hxx>
 class TopoDS_Face;
 class GeomAdaptor_HSurface;
 class GeomInt_LineConstructor;
@@ -49,7 +51,7 @@ public:
   //! Returns new WLine or null WLine if the number
   //! of the points is less than 2.
   Standard_EXPORT static
-    Handle(IntPatch_WLine) ComputePurgedWLine(const Handle(IntPatch_WLine)       &theWLine,
+    Handle(IntPatch_WLine) ComputePurgedWLine(const Handle(IntPatch_WLine)     &theWLine,
                                               const Handle(Adaptor3d_HSurface) &theS1,
                                               const Handle(Adaptor3d_HSurface) &theS2,
                                               const Handle(Adaptor3d_TopolTool)  &theDom1,
@@ -65,27 +67,20 @@ public:
 //! this point will be deleted.
   Standard_EXPORT static void JoinWLines(IntPatch_SequenceOfLine& theSlin,
                                          IntPatch_SequenceOfPoint& theSPnt,
-                                         const Standard_Real theTol3D,
-                                         const Standard_Real theU1Period,
-                                         const Standard_Real theU2Period,
-                                         const Standard_Real theV1Period,
-                                         const Standard_Real theV2Period,
-                                         const Standard_Real theUfSurf1,
-                                         const Standard_Real theUlSurf1,
-                                         const Standard_Real theVfSurf1,
-                                         const Standard_Real theVlSurf1,
-                                         const Standard_Real theUfSurf2,
-                                         const Standard_Real theUlSurf2,
-                                         const Standard_Real theVfSurf2,
-                                         const Standard_Real theVlSurf2);
-
+                                         Handle(Adaptor3d_HSurface) theS1,
+                                         Handle(Adaptor3d_HSurface) theS2,
+                                         const Standard_Real theTol3D);
 
 //! Extends every line from theSlin (if it is possible) to be started/finished
 //! in strictly determined point (in the place of joint of two lines).
 //! As result, some gaps between two lines will vanish.
 //! The Walking lines are supposed (algorithm will do nothing for not-Walking line)
-//! to be computed as a result of intersection of two quadrics.
-//! The quadrics definition is accepted in input parameters.
+//! to be computed as a result of intersection. Both theS1 and theS2 
+//! must be quadrics. Other cases are not supported.
+//! theArrPeriods must be filled as follows (every value must not be negative;
+//! if the surface is not periodic the period must be equal to 0.0 strictly):
+//! {<U-period of 1st surface>, <V-period of 1st surface>,
+//!               <U-period of 2nd surface>, <V-period of 2nd surface>}.
   Standard_EXPORT static void ExtendTwoWlinesToEachOther(IntPatch_SequenceOfLine& theSlin,
                                                          const IntSurf_Quadric& theS1,
                                                          const IntSurf_Quadric& theS2,
@@ -97,7 +92,7 @@ public:
                                                          const Bnd_Box2d& theBoxS1,
                                                          const Bnd_Box2d& theBoxS2);
 
-//! Max angle to concatenate two WLines to avoid result with C0-continuity
+  //! Max angle to concatenate two WLines to avoid result with C0-continuity
   static const Standard_Real myMaxConcatAngle;
 };
 
