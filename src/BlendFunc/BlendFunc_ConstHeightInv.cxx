ea4a31cc1d058659056bb974106588adfeec0983
jgv <jgv@opencascade.com>
2017-06-15 14:10:14 +0300

    Minor correction.

--- src/BlendFunc/BlendFunc_ConstHeightInv.cxx
+++ src/BlendFunc/BlendFunc_ConstHeightInv.cxx
@@ -1,7 +1,5 @@
-// Created on: 1996-06-06
-// Created by: Julia Gerasimova
-// Copyright (c) 1996-1999 Matra Datavision
-// Copyright (c) 1999-2014 OPEN CASCADE SAS
+// Created by: Julia GERASIMOVA
+// Copyright (c) 2015 OPEN CASCADE SAS
 //
 // This file is part of Open CASCADE Technology software library.
 //
