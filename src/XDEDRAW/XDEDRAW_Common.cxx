962dbe0c3be805efb429dca36a18d73cc6bf5d34
gka <gka@opencascade.com>
2018-02-26 16:27:46 +0300

    0029526: Test Harness command "ReadIges" does not support "read.iges.onlyvisible" mode
    In the Test Harness command "ReadIges" there is added taking into account value of the parameter "read.only.visible"

--- src/XDEDRAW/XDEDRAW_Common.cxx
+++ src/XDEDRAW/XDEDRAW_Common.cxx
@@ -192,10 +192,12 @@ static Standard_Integer ReadIges (Draw_Interpretor& di, Standard_Integer argc, c
   
   DeclareAndCast(IGESControl_Controller,ctl,XSDRAW::Controller());
   if (ctl.IsNull()) XSDRAW::SetNorm("IGES");
+  //get value of the Test Harness parameter "read.only.visible"
   Standard_Integer readOnlyVisible = Interface_Static::IVal("read.iges.onlyvisible");
  
   IGESCAFControl_Reader reader ( XSDRAW::Session(),Standard_True);
   reader.SetReadVisible(readOnlyVisible);
+
   if (argc == 4) {
     Standard_Boolean mode = Standard_True;
     for ( Standard_Integer i = 0; argv[3][i] ; i++ ) 
